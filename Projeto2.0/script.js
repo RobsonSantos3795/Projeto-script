fea805682ffe9c2ca4e7b4f7d082109a6c9444fe
Robson G. <123911094+RobsonSantos3795@users.noreply.github.com>
2024-05-27 19:00:57 -0300

    Projeto 2.0

---
+++ Projeto2.0/script.js
@@ -0,0 +1,17 @@
+/*var c = 1;
+while (c <= 2) {
+    console.log(`Imagem ${c}`);
+    c = c + 1;
+    //c++
+}
+*/
+
+/*var b = 1;
+do {
+    console.log(`Imagem ${b}`);
+    b++;
+}while (b <= 5)*/
+
+for (var a = 1 ; a <= 10; a++) {
+    console.log(`imagens 123..toExponential.! ${a}`);
+}
