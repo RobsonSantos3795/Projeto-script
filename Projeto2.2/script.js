9f6eee136727ef3d3b243b2c4093703ace1de1c9
Robson G. <123911094+RobsonSantos3795@users.noreply.github.com>
2024-07-23 02:30:12 -0300

    estudos variaveis compostas

--- Projeto2.2/script.js
+++ Projeto2.2/script.js
@@ -1,1 +1,12 @@
-var a = 1;
+let num = [5, 8, 7, 6, 4, 2];
+num.push(1); // Adiciona mais um
+num.sort();  // Ordena o vetor
+console.log(num); // Exibe o vetor
+console.log(`O valor tem ${num.length} posições`); // Exibe tamanho do vetor
+console.log(`O primeiro valor é o ${num[0]}`); // Exibe primeiro elemento do vetor
+let pos = num.indexOf(5);
+if (pos == -1) {
+    console.log('Não foi encontrado')
+} else {
+    console.log(`O valor está na posição ${pos}`)
+}
---
+++ Projeto2.3/script.js
@@ -0,0 +1,8 @@
+let valores = [8, 1, 7, 9, 4, 5, 3];
+valores.push(6);
+valores.sort();
+console.log(valores);
+
+for(let pos = 0; pos < valores.length; pos++) {
+    console.log(`A posição ${pos} tem o valor ${valores[pos]}`);
+}
